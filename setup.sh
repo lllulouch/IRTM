0546e5472a75dd77a2dc2fec3344e7c318329b04
JackywithaWhiteDog <b07705015@ntu.edu.tw>
2020-12-16 09:51:40 +0800

    test: test nbstripout

--- setup.sh
+++ setup.sh
@@ -9,6 +9,8 @@
   python -m pip install --no-cache-dir -r requirements.txt    || \
   python3 -m pip install --no-cache-dir -r requirements.txt )
 
+nbstripout --install --attributes .gitattributes
+
 mkdir data
 
 wget --load-cookies /tmp/cookies.txt -c "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1nhHoRAenENHJ3XU_f0rTjHlQHhg4HGx5' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1nhHoRAenENHJ3XU_f0rTjHlQHhg4HGx5" -O data/irtm.csv && rm -rf /tmp/cookies.txt
